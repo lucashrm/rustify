62fae4829b6d760acd547e6993d32dec8a471eec
yatss <lucas.herman@epitech.eu>
2025-01-16 23:41:53 +0100

    ♻️ Music display change

--- src/core/music.rs
+++ src/core/music.rs
@@ -1,26 +1,38 @@
 ﻿use std::io::Error;
 use id3::{Tag, TagLike};
+use std::path::Path;
+use mp3_duration;
+use vizia::prelude::*;
 
 #[derive(Debug)]
 pub struct MusicMetaData {
     title: String,
     artist: String,
     album: String,
+    length: f32,
 }
 
 impl MusicMetaData {
     fn new(filepath: &str) -> Result<Self, Error> {
         let tag = Tag::read_from_path(filepath).expect("error while reading path");
 
+        let path = Path::new(filepath);
+        let duration = mp3_duration::from_path(&path).unwrap();
+
         Ok(Self {
             artist: tag.artist().unwrap_or_else(|| "unknown artist").to_string(),
             title: tag.title().unwrap_or_else(|| "untitled").to_string(),
             album: tag.album().unwrap_or_else(|| "no album").to_string(),
+            length: duration.as_secs_f32()
         })
     }
 
     pub fn get_infos(&self) -> (&str, &str, &str) {
         (&self.title.as_str(), &self.album.as_str(), &self.artist.as_str())
+    }
+
+    pub fn get_duration(&self) -> f32 {
+        self.length
     }
 }
 
--- src/gui/library.rs
+++ src/gui/library.rs
@@ -1,16 +1,28 @@
-﻿use vizia::prelude::*;
+﻿use std::fmt::Debug;
+use vizia::icons::ICON_PLAYER_PLAY_FILLED;
+use vizia::prelude::*;
 use crate::core::music::Music;
 
-pub struct MusicLine {}
+pub struct MusicLine {
+    on_play: Option<Box<dyn Fn(&mut EventContext)>>,
+}
 
 impl View for MusicLine {}
 
 impl MusicLine {
     pub fn new(cx: &mut Context, music: &Music) {
+        let duration = format!("{}:{}",
+                               (music.metadata.get_duration() / 60.0) as i32,
+                               music.metadata.get_duration() % 60.0);
+
         HStack::new(cx, |cx| {
             Label::new(cx, music.metadata.get_infos().0);
             Label::new(cx, music.metadata.get_infos().1);
             Label::new(cx, music.metadata.get_infos().2);
+            Label::new(cx, duration);
+            Button::new(cx, |cx| {
+                Svg::new(cx, ICON_PLAYER_PLAY_FILLED)
+            });
         });
     }
 }
@@ -26,4 +38,3 @@ impl Library {
         })
     }
 }
-
